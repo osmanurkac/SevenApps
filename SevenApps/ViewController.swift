1e6eca3de0c06114db2943353302c54ddd2406e1
Osman Tarık Urkaç <osmantarikurkac@hotmail.com>
2025-02-12 20:54:24 +0300

    Initial Commit

---
+++ SevenApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenApps
+//
+//  Created by Osman Tarık Urkaç on 12.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SevenAppsTests/SevenAppsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SevenAppsTests.swift
+//  SevenAppsTests
+//
+//  Created by Osman Tarık Urkaç on 12.02.2025.
+//
+
+import Testing
+@testable import SevenApps
+
+struct SevenAppsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
